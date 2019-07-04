ed12f2dc350cf8e82c43c50067100ad49709503d
KamajiiSC <brandonlschaen@gmail.com>
2019-07-03 21:33:55 -0400

    Created event listener for glove-btn and created a glove() function

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ document.getElementById("train-display").innerHTML= "Training Points: " + trainP
 document.getElementById("glove-display").innerHTML= "Glove Level: " + gloveLvl;
 
 document.getElementById("train-btn").addEventListener("click", train);
+document.getElementById("glove-btn").addEventListener("click", glove);
 
 function train(){
   trainPoints++;
   document.getElementById("train-display").innerHTML= "Training Points: " + trainPoints;
   //alert (trainPoints);
+};
+
+function glove(){
+
 };
 
 
