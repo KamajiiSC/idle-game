3cd592ea907efa27c234fc83d79c2ce6f238261a
KamajiiSC <brandonlschaen@gmail.com>
2019-07-03 21:32:02 -0400

    Changed button id to singular, intialized gloveLvl and changed innerhtml of glove-display to gloveLvl

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let trainPoints = 0;
+let gloveLvl = 0;
 
 document.getElementById("train-display").innerHTML= "Training Points: " + trainPoints;
+document.getElementById("glove-display").innerHTML= "Glove Level: " + gloveLvl;
 
 document.getElementById("train-btn").addEventListener("click", train);
 
