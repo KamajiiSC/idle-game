7af19d2e9d7c5102a617ef71f0d35be5cb5f575a
KamajiiSC <brandonlschaen@gmail.com>
2019-07-04 22:51:39 -0400

    Commented code to increase readabilitiy

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ let trainPoints = 0;
 let gloveLvl = 1;
 let dummyLvl = 0;
 
+// Event Listeners for the buttons
 document.getElementById("train-btn").addEventListener("click", train);
 document.getElementById("glove-btn").addEventListener("click", glove);
 
+// Training function 
 function train(){
+  // Increments trainPoints by the gloveLvl each time "Train!" is pressed
   for (i= 0; i < gloveLvl; i++){
     trainPoints++;
   }
+  
+  // Updates the training points display
   document.getElementById("train-display").innerHTML= "Training Points: " + trainPoints;
-  //alert (trainPoints);
 };
 
+// Function to increment gloveLvl, if you have atleast 5 trainPoints it removes 5 and increments gloveLvl
 function glove(){
   if(trainPoints >= 5){
     trainPoints -= 5;
